83ae83e1e5b1939a0197d39625d9f84ff7cc00be
murasaki3 <skrk.o.3@gmail.com>
2018-07-03 16:26:36 +0900

    initial commit

---
+++ 2_functions/sakurako.py
@@ -0,0 +1,16 @@
+def calculate(num1,num2):
+
+    sum = num1+num2
+    difference = num1-num2
+    product = num1*num2
+    quotient = num1/num2
+    remainder = num1%num2
+
+    print(sum)
+    print(difference)
+    print(product)
+    print(quotient)
+    print(remainder)
+
+
+calculate(3,4)
