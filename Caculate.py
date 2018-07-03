1224d71b04ab7ecbce78b7e53f624e4d6382bd63
polaris <polaris@sd.tmu.ac.jp>
2018-07-03 15:53:51 +0900

    initial commit

---
+++ Caculate.py
@@ -0,0 +1,18 @@
+"""Caculation"""
+a = int(20)
+b = int(12)
+sum = a + b
+difference = a - b
+product = a*b
+quotient = int(a/b)
+remainder = a%b
+print(sum)
+print(type(sum))
+print(difference)
+print(type(difference))
+print(product)
+print(type(product))
+print(quotient)
+print(type(quotient))
+print(remainder)
+print(type(remainder))
