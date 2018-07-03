d42f34a46980cfbee8228bcb33a499dc5c065d82
polaris <polaris@sd.tmu.ac.jp>
2018-07-03 16:17:31 +0900

    initial commit

---
+++ function_caculate.py
@@ -0,0 +1,29 @@
+def Caculate(num1,num2):
+    sum = num1 + num2
+    difference = num1 - num2
+    product = num1*num2
+    quotient = num1/num2
+    remainder = num1%num2
+    
+    print(sum)
+    print(type(sum))
+    print(difference)
+    print(type(difference))
+    print(product)
+    print(type(product))
+    print(quotient)
+    print(type(quotient))
+    print(remainder)
+    print(type(remainder))
+
+if __name__  == "__main__":
+    temp1=input("Please insert number1:")
+    temp2=input("Please insert number2:")
+    if (temp1.isdigit() and temp2.isdigit()):
+        Caculate(int(temp1),int(temp2))
+    else:
+        print("You didn't input a number!")
+        
+    
+    
+    
