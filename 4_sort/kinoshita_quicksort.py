24bc21cfbfd1aeb3fcc869e4f81c8e29af80f744
Yuma Kinoshita <kinoshita-yuma@ed.tmu.ac.jp>
2018-07-23 22:08:54 +0900

    implemented quicksort

--- 4_sort/kinoshita_quicksort.py
+++ 4_sort/kinoshita_quicksort.py
@@ -8,20 +8,14 @@ def quicksort(list_):
 
 
 def recursive_quicksort(list_, start, stop):
-    if stop - start == 2:
-        if list_[stop-1] < list_[start]:
-            list_[start], list_[stop-1] = list_[stop-1], list_[start]
-        return
-    elif stop - start <= 1:
+    if stop - start <= 1:
         return
 
     med_idx = (start + stop - 1) // 2
     pivot = list_[med_idx]
     i = start
     j = stop - 1
-    print((start, stop))
     while True:
-        print("list: {0}, (i, j, pivot): {1}".format(list_, (i, j, pivot)))
         while list_[i] < pivot:
             i += 1
         while list_[j] > pivot:
@@ -29,13 +23,11 @@ def recursive_quicksort(list_, start, stop):
 
         if i >= j:
             break
-        else:
-            list_[i], list_[j] = list_[j], list_[i]
-
+        list_[i], list_[j] = list_[j], list_[i]
         i += 1
         j -= 1
 
-    recursive_quicksort(list_, start, i-1)
+    recursive_quicksort(list_, start, i)
     recursive_quicksort(list_, j+1, stop)
 
     return
@@ -51,6 +43,10 @@ if __name__ == "__main__":
     print(quicksort(list1) == sorted(list1))
 
     list1 = [-1, 0, 1e-10, float("inf"), -float("inf")]
+    print(quicksort(list1))
+    print(quicksort(list1) == sorted(list1))
+
+    list1 = [0, 3, 2, 0, 4, 7, 5]
     print(quicksort(list1))
     print(quicksort(list1) == sorted(list1))
 
